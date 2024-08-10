0529b80e4a82b8cc0225e3b410c661c85bb9ce36
PurplProto <pairedprototype@hotmail.com>
2024-08-10 22:35:36 +0100

    Seperated assets, popup and content-scripts into seperate Nx projects and fixed build output by switching to ESBuild to enable bundling with vendor code included

--- apps/content-scripts/vite.config.ts
+++ apps/content-scripts/vite.config.ts
@@ -4,7 +4,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: './node_modules/.vite/ois',
+  cacheDir: '../../node_modules/.vite/ois',
 
   plugins: [nxViteTsPaths()],
 
@@ -19,6 +19,6 @@ export default defineConfig({
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
-    coverage: { reportsDirectory: './coverage/ois', provider: 'v8' },
+    coverage: { reportsDirectory: '../../coverage/ois', provider: 'v8' },
   },
 });
