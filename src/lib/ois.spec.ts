75cc35390b8ce9cd4f22bfe3fe646c7adbfc1aed
PurplProto <pairedprototype@hotmail.com>
2024-08-11 02:23:35 +0100

    Restructured and converted source to typescript

--- src/lib/ois.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ois } from './ois';
-
-describe('ois', () => {
-  it('should work', () => {
-    expect(ois()).toEqual('ois');
-  });
-});
--- src/lib/ois.ts
+++
@@ -1,3 +0,0 @@
-export function ois(): string {
-  return 'ois';
-}
