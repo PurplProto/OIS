6969e521ce41be9aaa49d559b0fd32ec602fddc7
PurplProto <pairedprototype@hotmail.com>
2024-08-11 02:23:34 +0100

    Massive clean up - practically a rewrite

--- main/openInSteam.js
+++ main/openInSteam.js
@@ -33,14 +33,14 @@ function getPageId(urlString, regexString) {
 
 // Function to add button to right of the page title,
 // passing in the page type so the URL can be set correctly
-function addOSIButton(steamURL, pageId) {
+function addOISButton(steamURL, pageId) {
     const otherSiteInfoBar = document.getElementsByClassName("apphub_OtherSiteInfo");
     const workShopInfoBar = document.getElementsByClassName("workshopItemControlCtn");
     const profileInfoBar = document.getElementsByClassName("profile_header_actions");
-    const osiLogo = getExtensionURL("icons/ois_logo.png");
+    const oisLogo = getExtensionURL("icons/ois_logo.png");
 
     // Prevent attempting to add a button on a non-supported browser
-    if (osiLogo == null) {
+    if (oisLogo == null) {
         return;
     }
 
@@ -50,11 +50,11 @@ function addOSIButton(steamURL, pageId) {
         imgElement,
         parentElement,
     ) {
-        linkElement.classList.add("osi_link");
+        linkElement.classList.add("ois_link");
 
         spanElement.appendChild(imgElement);
         linkElement.appendChild(spanElement);
-        parentElement.insertBefore(linkElement, parentElement.children[0]);
+        parentElement.insertBefore(linkElement, parentElement.children[2]);
     }
 
     // Top menu bar
@@ -64,8 +64,8 @@ function addOSIButton(steamURL, pageId) {
         const topBoxImg = document.createElement("img");
         const topDiv = otherSiteInfoBar[0];
 
-        topBoxImg.className = "img_osi_menu_top";
-        topBoxImg.src = osiLogo;
+        topBoxImg.className = "img_ois_menu_top";
+        topBoxImg.src = oisLogo;
 
         topBox.className = "btnv6_blue_hoverfade btn_medium";
         topBox.href = steamURL + pageId;
@@ -80,8 +80,8 @@ function addOSIButton(steamURL, pageId) {
         const lowerBoxImg = document.createElement("img");
         const lowerDiv = workShopInfoBar[2];
 
-        lowerBoxImg.className = "img_osi_menu_lower";
-        lowerBoxImg.src = osiLogo;
+        lowerBoxImg.className = "img_ois_menu_lower";
+        lowerBoxImg.src = oisLogo;
 
         lowerBox.className = "general_btn";
         lowerBox.href = steamURL + pageId;
@@ -96,8 +96,8 @@ function addOSIButton(steamURL, pageId) {
         const profileBoxImg = document.createElement("img");
         const profileDiv = profileInfoBar[0];
 
-        profileBoxImg.className = "img_osi_workshop_bar";
-        profileBoxImg.src = osiLogo;
+        profileBoxImg.className = "img_ois_workshop_bar";
+        profileBoxImg.src = oisLogo;
 
         profileBox.className = "btn_profile_action btn_medium";
         profileBox.href = steamURL + pageId;
@@ -106,58 +106,63 @@ function addOSIButton(steamURL, pageId) {
     }
 }
 
-if (pageLocation.href.match(/store\.steampowered\.com\/app/)) { // Steam store page
-    addOSIButton(
-        "steam://store/",
-        getPageId(
-            pageLocation.pathname,
-            /\d+/,
-        ),
-    );
-} else if (pageLocation.href.match(/steamcommunity\.com\/app\/\d+\/workshop/)) { // Steam workshop page
-    addOSIButton(
-        "steam://url/SteamWorkshopPage/",
-        getPageId(
-            pageLocation.search,
-            /\d+/,
-        ),
-    );
-} else if (pageLocation.href.match(/steamcommunity\.com\/sharedfiles\/filedetails\/\?id=\d+/)) { // Steam app item
-    addOSIButton(
-        "steam://url/CommunityFilePage/",
-        getPageId(
-            pageLocation.search,
-            /\d+/,
-        ),
-    );
-} else if (pageLocation.href.match(/steamcommunity\.com\/profiles\/\d+/)) { // Steam user profile
-    addOSIButton(
-        "steam://url/SteamIDPage/",
-        getPageId(
-            pageLocation.pathname,
-            /\d+/,
-        ),
-    );
-} else if (pageLocation.href.match(/steamcommunity\.com\/id/)) { // Steam user custom profile URL
-    const resolveIdUrl = `https://sirs.pairedprototype.com/resolvesteamid/${getPageId(pageLocation.pathname, /[a-zA-Z1-9]*$/)}`;
+function main() {
+    if (pageLocation.href.match(/store\.steampowered\.com\/app/)) { // Steam store page
+        addOISButton(
+            "steam://store/",
+            getPageId(
+                pageLocation.pathname,
+                /\d+/,
+            ),
+        );
+    } else if (pageLocation.href.match(/steamcommunity\.com\/workshop/)) { // Steam workshop page
+        addOISButton(
+            "steam://url/CommunityFilePage/",
+            getPageId(
+                pageLocation.search,
+                /\d+/,
+            ),
+        );
+    } else if (pageLocation.href.match(/steamcommunity\.com\/sharedfiles\/filedetails\/\?id=\d+/)) { // Steam app item
+        addOISButton(
+            "steam://url/CommunityFilePage/",
+            getPageId(
+                pageLocation.search,
+                /\d+/,
+            ),
+        );
+    } else if (pageLocation.href.match(/steamcommunity\.com\/profiles\/\d+/)) { // Steam user profile
+        addOISButton(
+            "steam://url/SteamIDPage/",
+            getPageId(
+                pageLocation.pathname,
+                /\d+/,
+            ),
+        );
+    } else if (pageLocation.href.match(/steamcommunity\.com\/id/)) { // Steam user custom profile URL
+        const resolveIdUrl = `https://sirs.pairedprototype.com/resolvesteamid/${getPageId(pageLocation.pathname, /[a-zA-Z1-9]*$/)}`;
 
-    addOSIButton(
-        null,
-        null,
-    );
+        addOISButton(
+            null,
+            null,
+        );
 
-    httpGetAsync(
-        resolveIdUrl,
-        (steamUserIdResponse) => {
-            const steamUserIdJson = JSON.parse(steamUserIdResponse.response);
-            const osiButtonLink = document.getElementsByClassName("osi_link");
+        httpGetAsync(
+            resolveIdUrl,
+            (steamUserIdResponse) => {
+                const steamUserIdJson = JSON.parse(steamUserIdResponse.response);
+                const oisButtonLink = document.getElementsByClassName("ois_link");
 
-            if (steamUserIdResponse.status === 200
-                    && steamUserIdJson.error.errorStatusCode === 0) {
-                osiButtonLink[0].href = `steam://url/SteamIDPage/${steamUserIdJson.response.steamIdData.steamIdValue}`;
-            } else {
-                osiButtonLink[0].parentNode.removeChild(osiButtonLink);
-            }
-        },
-    );
+                if (steamUserIdResponse.status === 200
+                        && steamUserIdJson.error.errorStatusCode === 0) {
+                    oisButtonLink[0].href = `steam://url/SteamIDPage/${steamUserIdJson.response.steamIdData.steamIdValue}`;
+                } else {
+                    oisButtonLink[0].parentNode.removeChild(oisButtonLink);
+                }
+            },
+        );
+    }
 }
+
+document.onload = main();
+
